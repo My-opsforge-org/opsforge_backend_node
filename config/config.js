805a7c5eafbb0b3c3479ef910d2d47f3c648186f
Aftab Khan <khanaftaba1@gmail.com>
2025-07-01 22:54:11 -0400

    trigger

--- config/config.js
+++ config/config.js
@@ -18,11 +18,13 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST || '127.0.0.1',
-    port: process.env.POSTGRES_PORT || 5432,
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }; 
