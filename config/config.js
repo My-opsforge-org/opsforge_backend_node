c667c18ed35ef5514e2af2d079f1aa155875f653
Aftab Khan <khanaftaba1@gmail.com>
2025-07-01 23:47:20 -0400

    trigger

--- config/config.js
+++ config/config.js
@@ -2,24 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    port: process.env.POSTGRES_PORT || 5432,
+    dialect: 'postgres'
   },
   test: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    port: process.env.POSTGRES_PORT || 5432,
+    dialect: 'postgres'
   },
   production: {
     use_env_variable: 'DATABASE_URL',
